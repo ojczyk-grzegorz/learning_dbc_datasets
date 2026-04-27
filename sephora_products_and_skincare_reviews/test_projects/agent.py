92b517720441818abc67424625083f1612ad3928
ojczyk-grzegorz <ojczyk.grzegorz@gmail.com>
2026-04-27 06:55:19 +0000

    first agent

---
+++ sephora_products_and_skincare_reviews/test_projects/agent.py
@@ -0,0 +1,115 @@
+import os
+from uuid import uuid4
+from typing import Any
+
+import yaml
+import mlflow
+from mlflow.pyfunc import ResponsesAgent
+from mlflow.types.responses import ResponsesAgentRequest, ResponsesAgentResponse
+
+from langchain.agents import create_agent
+from databricks_langchain import ChatDatabricks, VectorSearchRetrieverTool
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.state import CompiledStateGraph
+
+
+LLM_ENDPOINT_NAME = "databricks-gpt-oss-120b"
+INDEX_NAME = "workspace.sephora_products_and_skincare_reviews.bronze_pdf_chunked_index"
+SYSTEM_PROMPT = """You are expert on Sephora's pricing strategy. Respond in a clear, professional, and factual tone
+    appropriate for engineers and technical staff. Use only verified information from the provided context, include 
+    reference to every information you provide. Do not hallucinate information. If you are unsure about the answer,
+    say "I don't know"."""
+
+
+def _load_config(path: str = "agent_config.yaml") -> dict[str, Any]:
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Config file not found at path: {path}")
+    with open(path, 'r', encoding='utf-8') as f:
+        cfg = yaml.safe_load(f) or {}
+    
+    llm_endpoint_name = cfg["llm_endpoint_name"]
+    
+    vs: dict = cfg["vector_search"]
+    index_name =vs["index_name"]
+    system_prompt = vs["system_prompt"]   
+    num_results = int(vs.get("num_results", 5))
+    
+    return {
+        "llm_endpoint_name": llm_endpoint_name,
+        "vs_index_name": index_name,
+        "vs_system_prompt": system_prompt,
+        "vs_num_results": num_results,
+    }
+
+
+def build_agent(llm_endpoint: str, index_name: str, system_prompt: str, num_results: int = 3) -> CompiledStateGraph:
+    model = ChatDatabricks(
+        endpoint=llm_endpoint,
+        max_tokens=500
+    )
+
+    vs_tool = VectorSearchRetrieverTool(
+        index_name=index_name,
+        description="Sephora pricing strategy",
+        num_results=num_results,
+    )
+
+    checkpointer = InMemorySaver()
+
+    agent = create_agent(
+        model=model,
+        tools=[vs_tool],
+        system_prompt=system_prompt,
+        checkpointer=checkpointer,
+    )
+
+    return agent
+
+
+def _last_user_text(messages: list[dict[str, Any]]) -> str:
+    user_msgs = [m for m in messages if (m.get("role") == "user")]
+    if user_msgs:
+        return user_msgs[-1].get("content", "")
+    elif messages:
+        return str(messages[-1].get("content", ""))
+    return ""
+
+
+class LangChainResponsesAgent(ResponsesAgent):
+    def __init__(self):
+        cfg = _load_config()
+        self._cfg = cfg
+        self._agent = build_agent(
+            llm_endpoint=cfg["llm_endpoint_name"],
+            index_name=cfg["vs_index_name"],
+            system_prompt=cfg["vs_system_prompt"],
+            num_results=cfg["vs_num_results"],
+        )
+
+    def predict(self, request: ResponsesAgentRequest) -> ResponsesAgentResponse:
+        msgs = [m.model_dump() for m in request.input]
+        if not msgs:
+            raise ValueError("No messages!")
+        _ = _last_user_text(msgs)
+
+        thread_id = f"oka-{uuid4()}"
+
+        results = self._agent.invode(
+            {"messages": msgs},
+            config={
+                "configurable": {"thread_id": thread_id}
+            }
+        )
+
+        try:
+            text = results["messages"][-1].content
+        except Exception:
+            text = str(results)
+
+        return ResponsesAgentResponse(
+            output=[self.create_text_output_item(text, str(uuid4()))],
+            custom_outputs=request.custom_inputs
+        )
+    
+agent = LangChainResponsesAgent()
+mlflow.models.set_model(agent)
